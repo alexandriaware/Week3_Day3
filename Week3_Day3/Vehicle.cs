6c91749fac786810d8971721d1511c1fba1b54bf
alexandriaware <ali.mercer92@gmail.com>
2017-10-11 11:20:55 -0400

    added constructors

--- Week3_Day3/Vehicle.cs
+++ Week3_Day3/Vehicle.cs
@@ -8,6 +8,7 @@ namespace Week3_Day3
 {
     class Vehicle
     {
+        //fields
         private int seats;
         private int carryingCapacity;
         private string color;
@@ -15,19 +16,23 @@ namespace Week3_Day3
         private float gearRatio;
         private float engineSize;
 
-        public int Seats
-        {
-            get { return this.seats; }
-        }
 
-        public int CarryingCapacity
-        {
-            get { return this.carryingCapacity; }
-        }
-
+        //Properties
+        public int Seats { get; }
+       
+        public int CarryingCapacity { get; }
+        
         public string Color { get; set; }
 
 
+        //Constructors
+        public Vehicle (int seats, int carryingCapacity, string color, double movementSpeed)
+        {
+            this.seats = seats;
+            this.carryingCapacity = carryingCapacity;
+            this.color = color;
+            this.movementSpeed = movementSpeed;
+        }
 
     }
 }
