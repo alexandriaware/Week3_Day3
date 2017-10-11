3dc5874f1d2f08fca74cb1847ef1f9e40681ddd4
alexandriaware <ali.mercer92@gmail.com>
2017-10-11 11:15:12 -0400

    added properties and fields

---
+++ Week3_Day3/Vehicle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_Day3
+{
+    class Vehicle
+    {
+        private int seats;
+        private int carryingCapacity;
+        private string color;
+        private double movementSpeed;
+        private float gearRatio;
+        private float engineSize;
+
+        public int Seats
+        {
+            get { return this.seats; }
+        }
+
+        public int CarryingCapacity
+        {
+            get { return this.carryingCapacity; }
+        }
+
+        public string Color { get; set; }
+
+
+
+    }
+}
