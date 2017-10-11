7457a46db75708c6ef9fb2f346df85776a8a4943
alexandriaware <ali.mercer92@gmail.com>
2017-10-11 14:14:46 -0400

    completed the automobile constructor

--- Week3_Day3/Automobile.cs
+++ Week3_Day3/Automobile.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace Week3_Day3
 {
-    class Automobile
+    class Automobile : Vehicle
     {
+        //fields
+        private int numberOfWheels;
+        private float wheelSize;
+        private int numberOfDoors;
+
+        //Properties
+        public int NumberOfWheels
+        {
+            get { return this.numberOfWheels; }
+        }
+
+        //Constructor
+        public Automobile (int numberOfWheels, float wheelSize, int numberOfDoors, int seats, int carryingCapacity, string color, double movementSpeed)
+        {
+            this.numberOfWheels = numberOfWheels;
+            this.wheelSize = wheelSize;
+            this.numberOfDoors = numberOfDoors;
+            this.seats = seats;
+            this.color = color;
+            this.movementSpeed = movementSpeed;
+        }
+
     }
 }
